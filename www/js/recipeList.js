f78aa929a175985f0a5ceaaf047e4d21a9442a38
abid <abid>
2019-02-21 21:34:51 +0500

    some changes

--- www/js/recipeList.js
+++ www/js/recipeList.js
@@ -7,7 +7,7 @@ function getTableData() {
 function generateTable(tableData) {
 	var tBody = "";
 	for (var i = 0; i < tableData.length; i++) {
-			tBody+= "<tr><td>"+tableData[i].location_name+"</td><td>" + tableData[i].name + "</td><td>" +tableData[i].unit+"</td><td>" +tableData[i].quantity+"</td></tr>";
+			tBody+= "<tr><td>"+tableData[i].location_name+"</td><td>" + tableData[i].ing_name + "</td><td>" +tableData[i].quantity+"</td><td>" +tableData[i].unit+"</td></tr>";
 	}
 	  $("table tbody").append(tBody);
 }
